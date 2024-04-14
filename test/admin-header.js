7574bd6fc13e93b194757b3d1408d2a34a61d157
Tausarpiah <157867480+Tausarpiah@users.noreply.github.com>
2024-04-14 17:57:49 +0800

    Added some pages to test folder
    - added trng.htm
    - added admin.htm
    - added nco-dir.htm
    - added contact.htm
    - updated exco.htm (put up a notice)
    - added exco images
    - changed gary's picture upon his  request
    - updated and esp. FINALLY FIXED THE BLODDY FOOTER VMIN THINGY
    - cropped kai ze's picture
    - changed footer

---
+++ test/admin-header.js
@@ -0,0 +1,45 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<!DOCTYPE HTML>
+<HTML LANG='EN-GB'>
+    <HEAD>
+        <META charset="EBCDIC">
+        <LINK REL='stylesheet' HREF='style.css'>
+        <TITLE>Why are you here</TITLE>
+    <STYLE>
+    </STYLE>
+</HEAD>
+<BODY>
+    <HEADER>
+        <img src="cross.png">
+        <div>
+            <h1>Maris Stella High School St John's Brigade</h1>
+            <div class="top-nav">
+                <a href="index.html">Home</a>
+                <div class="dd">
+                    <button class="dd-btn" style="background-color: #F2F2F2; color: black;">Admin</button>
+                    <div class="dd-c" style="border-top: 0">
+                        <a href="nco-dir.htm">NCO Drive Directory</a>
+                        <a href="exco.htm">Leadership</a>
+                        <a href="schedule.htm">Schedule</a>
+                    </div>
+                </div>
+                <div class="dd">
+                    <button class="dd-btn">Training</button>
+                    <div class="dd-c" style="border-top: 0">
+                        <a href="fa-manual.pdf">F.A. Manual (PDF)</a>
+                        <a href="images.htm">Images</a>
+                    </div>
+                </div>
+                <a href="contact.htm">Contact</a>
+                <a href="bs.htm">Other</a>
+            </div>
+        </div>
+    </HEADER>
+</BODY>
+</HTML>
+
+`;
+
+document.body.appendChild(template.content);
---
+++ test/footer.js
@@ -0,0 +1,25 @@
+const template2 = document.createElement('template');
+
+template.innerHTML = `
+<!DOCTYPE HTML>
+<HTML LANG="EN-GB">
+    <HEAD>
+        <META charset="UTF-8">
+        <LINK REL='stylesheet' HREF='style.css'>
+        <TITLE>Why are you here</TITLE>
+    </HEAD>
+    <BODY>
+    <footer>
+        <a href="#top">Back to the top</a>
+        <a href="index.html">Main Page</a>
+        <a href="nco-dir.htm">NCO Drive</a>
+        <a href="exco.htm">Leadership</a>
+        <a href="contact.htm">Contact</a>
+    <div class="credits">Maintained by CPL Ja Zun</div>
+    </footer>
+    </BODY>
+</HTML>
+
+`;
+
+document.body.appendChild(template2.content);
